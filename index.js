00b69e6d1413a1bcf5fce7576438f05dc76fefdb
Georg Weber <guru123@gmx.at>
2018-09-13 19:37:28 +0000

    Add framework7 lib in index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    app.import('node_modules/framework7/js/framework7.js');
+  }
 };
